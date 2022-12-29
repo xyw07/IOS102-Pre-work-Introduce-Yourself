3b75dc571871b33e7288c82cf2410bad0986ae94
xyw0719 <wang.xinyue2@northeastern.edu>
2022-12-28 16:22:19 -0800

    Initial Commit

---
+++ IOS102-Pre-work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS102-Pre-work
+//
+//  Created by Xinyue Wang new mac on 12/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
